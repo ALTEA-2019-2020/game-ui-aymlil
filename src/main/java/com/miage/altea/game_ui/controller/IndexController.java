0273272fe3b867af5e04c7d576db8932f402ac0d
Aymerick <aymerick@Mac-de-Aymerick.local>
2020-02-03 09:43:29 +0100

    Commit de 9h45

--- src/main/java/com/miage/altea/game_ui/controller/IndexController.java
+++ src/main/java/com/miage/altea/game_ui/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.miage.altea.game_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,7 +20,7 @@ public class IndexController {
         return name;
     }
 
-    @GetMapping(value="/registerTrainer")
+    @PostMapping(value="/registerTrainer")
     ModelAndView registerNewTrainer(String trainerName){
         var modelAndView = new ModelAndView("register");
         modelAndView.addObject("name", trainerName);
